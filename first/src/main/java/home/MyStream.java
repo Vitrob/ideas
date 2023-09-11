c7cd174528e2bef968d84e4fd53fc28a4f3e5689
Vitrob <88090808+Vitrob@users.noreply.github.com>
2023-09-11 14:40:19 +0200

    Several fixes

--- first/src/main/java/home/MyStream.java
+++ first/src/main/java/home/MyStream.java
@@ -3,6 +3,8 @@ package home;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @ToString
 public class MyStream {
@@ -15,5 +17,22 @@ public class MyStream {
 
     public void add(final List<String> flowers) {
         this.flowers.addAll(flowers);
+    }
+
+    public void modify() {
+        flowers.stream()
+                .map((String::toUpperCase))
+                .filter(flower -> flower.startsWith("S"))
+                .distinct()
+                .forEach(System.out::println);
+    }
+
+    public Optional<Integer> multiply(Integer value) {
+        return Optional.ofNullable(value)
+                .map(v -> v * 100);
+    }
+
+    public Integer getRandomNumber() {
+        return  new Random().nextInt(10 - 1 + 1) + 1;
     }
 }
